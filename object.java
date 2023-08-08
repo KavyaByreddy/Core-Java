4e7e29eebb6a65b53424f92a20caa3aa226bf844
KavyaByreddy <kavyabyreddy68@gmail.com>
2023-08-08 23:29:29 +0530

    k

---
+++ object.java
@@ -0,0 +1,9 @@
+public class object {
+    int i=10;
+    String s="kavya";
+ public static void main(String[] args) {
+    object s1=new object();
+    System.out.println(s1.i);
+    System.out.println(s1.s);
+    }
+}
