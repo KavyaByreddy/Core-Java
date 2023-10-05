54b841612ed26ce77bc1af10e61abf17edcd6f5e
KavyaByreddy <kavyabyreddy68@gmail.com>
2023-10-05 23:36:08 +0530

    k

---
+++ Basics/src/basicprograms/Mergesort.java
@@ -0,0 +1,35 @@
+package basicprograms;
+
+public class Mergesort {
+	public static int[ ] merge(int[ ] arr1,int[] arr2) {
+		int l1=arr1.length;
+		int l2=arr2.length;
+		int [ ] m=new int[l1+l2];
+		int i=0,j=0,k=0;
+		while(i<l1&&j<l2) {
+			if(arr1[i]<arr2[j]) {
+				m[k++]=arr1[i++];
+			}
+			else {
+				m[k++]=arr2[j++];
+			}
+	}
+	while(i<l1) {
+			m[k++]=arr1[i++];
+		}
+		while(j<l2) {
+			m[k++]=arr2[j++];
+		}
+		return m;
+	}
+
+	public static void main(String[] args) {
+		int[ ]a1= {2,5,7,9};
+		int[ ]a2= {1,6,8};
+		int[ ]b= merge(a1,a2);
+		for(int i=0;i<b.length;i++) {
+			System.out.println(b[i]);
+		}
+
+	}
+}
--- Basics/src/basicprograms/Vowels.java
+++ Basics/src/basicprograms/Vowels.java
@@ -4,31 +4,10 @@ public class Vowels {
 
     public static void main(String[] args) {
 
-        char ch = 'i';
-
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " is vowel");
-        else
-            System.out.println(ch + " is consonant");
-
-    }
-}
-/*public class Vowels {
-
-    public static void main(String[] args) {
-
-        char ch = 'z';
-
-        switch (ch) {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-                System.out.println(ch + " is vowel");
-                break;
-            default:
-                System.out.println(ch + " is consonant");
+        String s = "java developer";
+        for(int i=0;i<s.length();i++) {
+        if(s.charAt(i)=='a'|| s.charAt(i)=='e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' ||s.charAt(i) == 'u' )
+            System.out.println(s.charAt(i));
         }
     }
-}*/
+}
