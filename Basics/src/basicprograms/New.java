b4f2e191bdc4e45d4a85e8a891a4ce446683fd53
KavyaByreddy <kavyabyreddy68@gmail.com>
2023-10-14 06:25:26 +0530

    k

---
+++ Basics/src/basicprograms/New.java
@@ -0,0 +1,29 @@
+package basicprograms;
+
+public class New {
+
+    class Customer {
+
+        public int custId;
+
+        public String custName;
+
+ }
+
+  
+
+ public class Test01 {
+
+       
+
+        public static void main(String[] args) {
+
+              Customer c1 = new New();
+
+              Customer c2 = c1;
+
+              Customer c3 = new Customer();
+
+              Customer c4 = c2;
+
+        }}}
